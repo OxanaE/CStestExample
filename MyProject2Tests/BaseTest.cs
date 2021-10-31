085eb8192f23993d34122c3db078e8d56a90c2fa
Oxana Ermolaev <ONErmolaev@gmail.com>
2021-10-30 23:18:17 -0400

    Add project files.

---
+++ MyProject2Tests/BaseTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace MyProject2.Tests
+{
+    public abstract class BaseTest
+    {
+        private IWebDriver driver;
+
+        public IWebDriver Driver {
+            get { return driver; }
+        }
+
+        [SetUp]
+        public void Before() {
+
+            this.driver = new ChromeDriver("c:\\app\\WebDriver");
+            this.driver.Manage().Timeouts().ImplicitWait = new System.TimeSpan(0, 0, 5);
+        }
+
+        [TearDown]
+        public void After() {
+            if (driver != null) {
+                driver.Close();
+                driver.Quit();
+            }
+        }
+    }
+}
---
+++ MyProject2Tests/HelloWorldTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace MyProject2.Tests
+{
+     public class HelloWorldTests : BaseTest
+    {
+        [Test]
+        public void MainTest()
+        {
+            IWebDriver driver = Driver;
+
+            driver.Url = "https://www.demoqa.com/books";
+
+            Driver.FindElement(By.XPath("//button[@id='login']")).Click();
+
+            driver.FindElement(By.Id("userName")).SendKeys("testuser_1");
+
+            driver.FindElement(By.Id("password")).SendKeys("Test@123");
+
+            driver.FindElement(By.Id("login")).Click();
+
+            IWebElement buttonLogOut = driver.FindElement(By.XPath("//button[@id='submit' and text()='Log out']"));
+            Assert.AreEqual("Log out", buttonLogOut.Text);
+        }
+    }
+}
